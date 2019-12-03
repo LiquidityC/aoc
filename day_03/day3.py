5949179463244cc55e0de409bea7183bcc1e4879
Linus Probert <linus.probert@verisure.com>
2019-12-03 09:01:08 +0100

    Solved day 3

--- day_03/day3.py
+++ day_03/day3.py
@@ -11,17 +11,41 @@ class OP:
     def __repr__(self):
         return "%s%d" % (self.direction, self.steps)
 
-def matrixCheckPos(matrix, pos, wireNum):
-    if not pos[0] in matrix:
-        matrix[pos[0]] = {}
-    if not pos[1] in matrix[pos[0]]:
-        matrix[pos[0]][pos[1]] = (".", set(wireNum))
-    elif pos[1] in matrix[pos[0]]:
-        entry = matrix[pos[0]][pos[1]]
-        if not wireNum in entry[1]:
-            matrix[pos[0]][pos[1]] = ("x", set(wireNum))
-        elif wireNum in entry[1]:
-            matrix[pos[0]][pos[1]][1].add(wireNum)
+def matrixCheckPos(matrix, pos, wireNum, steps):
+    tpos = (pos[0], pos[1])
+    if not tpos in matrix:
+        matrix[tpos] = { wireNum: steps }
+    else:
+        if not wireNum in matrix[tpos]:
+            matrix[tpos][wireNum] = steps;
+
+def populateMatrix(matrix, ops, wireNum):
+    pos = [0, 0]
+    stepCount = 0
+    for op in ops:
+        if op.direction == "U":
+            for i in range(op.steps):
+                stepCount += 1;
+                pos[0] += 1
+                matrixCheckPos(matrix, pos, wireNum, stepCount)
+        elif op.direction == "D":
+            for i in range(op.steps):
+                stepCount += 1;
+                pos[0] -= 1
+                matrixCheckPos(matrix, pos, wireNum, stepCount)
+        elif op.direction == "R":
+            for i in range(op.steps):
+                stepCount += 1;
+                pos[1] += 1
+                matrixCheckPos(matrix, pos, wireNum, stepCount)
+        elif op.direction == "L":
+            for i in range(op.steps):
+                stepCount += 1;
+                pos[1] -= 1
+                matrixCheckPos(matrix, pos, wireNum, stepCount)
+        else:
+            print("Bad direction:", op)
+            sys.exit(1)
 
 data1 = ""
 data2 = ""
@@ -40,26 +64,37 @@ wop2 = []
 for ostr in path2:
     wop2.append(OP(ostr[:1], int(ostr[1:])))
 
-matrix = { 0: { 0: "."}}
-pos = [0, 0]
-for op in wop1:
-    if op.direction == "U":
-        for i in range(op.steps+1):
-            pos[0] += 1
-            matrixCheckPos(matrix, pos, 1)
-    elif op.direction == "D":
-        for i in range(op.steps+1):
-            pos[0] -= 1
-            matrixCheckPos(matrix, pos, 1)
-    elif op.direction == "R":
-        for i in range(op.steps+1):
-            pos[1] += 1
-            matrixCheckPos(matrix, pos, 1)
-    elif op.direction == "L":
-        for i in range(op.steps+1):
-            pos[1] -= 1
-            matrixCheckPos(matrix, pos, 1)
-    else:
-        print("Bad direction:", op)
-        sys.exit(1)
+# Populate a lazy matrix
+matrix = { (0, 0): set([1, 2]) }
+populateMatrix(matrix, wop1, 1)
+populateMatrix(matrix, wop2, 2)
 
+
+closetsPos = (0, 0)
+minDist = sys.maxsize
+
+# Find closest intersection
+for k in matrix.keys():
+    if k[0] == 0 and k[1] == 0:
+        continue
+    if 1 in matrix[k] and 2 in matrix[k]:
+        if abs(k[0]) + abs(k[1]) < minDist:
+            minDist = abs(k[0]) + abs(k[1])
+            closestPos = k
+
+print("Closest pos:", closestPos)
+print("Min dist: %d" % minDist)
+
+closestWalkPos = (0, 0)
+closestWalkDist = sys.maxsize
+# Find closest intersection by steps
+for k in matrix.keys():
+    if k[0] == 0 and k[1] == 0:
+        continue
+    if 1 in matrix[k] and 2 in matrix[k]:
+        if matrix[k][1] + matrix[k][2] < closestWalkDist:
+            closestWalkDist = matrix[k][1] + matrix[k][2]
+            closestWalkPos = k
+
+print("Closest walk pos:", closestWalkPos)
+print("Closest walk dist: %d" % closestWalkDist)
