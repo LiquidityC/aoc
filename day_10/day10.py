4cafb1f2bef3d445ed558258f99e79407cc889b5
Linus Probert <linus.probert@verisure.com>
2019-12-10 15:58:25 +0100

    Completes day 10

--- day_10/day10.py
+++ day_10/day10.py
@@ -1,41 +1,46 @@
+import math
+import pprint
 
-class Asteroid :
-
-    def __init__(self, pos, dim):
-        self.pos = pos
-        self.dim = dim
-        self.los = {}
-
-    def __repr__(self):
-        return "(%d, %d, %d, %d)" % (self.pos[0], self.pos[1], self.dim[0], self.dim[1])
-
-def getAsteroidsFromLines(lines):
-    x = 0
-    y = 0
-    asteroids = []
-    for line in lines:
-        for c in line:
-            if not c == "#":
-                continue
-            asteroids.append(Asteroid((x+4, y+4), (2, 2)))
-            x += 10
-        y += 10
-    return (10+len(lines[0])*10, 10+len(lines)*10), asteroids
-
-def traceRay(asteroids, mapDim, vector):
-    # This should return the first asteroid the vector hits
-    pass
-
-with open("input.txt", "r") as fh:
-    (mapDim, asteroids) = getAsteroidsFromLines(fh.readlines())
-
-for a in asteroids:
-    for b in asteroids:
-        if b.pos in a.los:
+def countAsteroidsInLOS(station, asteroids):
+    detected = set()
+    for asteroid in asteroids:
+        if asteroid == station:
             continue
-        obj = traceRay(asteroids, mapDim, (a.pos, b.pos))
-        if obj and not obj.pos in a.los:
-            a.los[obj.pos] = obj
-            obj.los[a.pos] = a
+        dx, dy = asteroid[0]-station[0], asteroid[1]-station[1]
+        g = abs(math.gcd(dx, dy))
+        detected.add((dx//g, dy//g))
+    return detected
 
-print(mapDim)
+
+with open("input.txt") as fh:
+    data = [s.strip() for s in fh.readlines()]
+
+asteroids = set()
+for y in range(len(data)):
+    for x in range(len(data[y])):
+        if data[y][x] == "#":
+            asteroids.add((x, y))
+
+counts = []
+for station in asteroids:
+    inLOS = countAsteroidsInLOS(station, asteroids)
+    counts.append((len(inLOS), station, inLOS))
+counts.sort(reverse=True)
+amtInLOS, station, inLOS = counts[0]
+print("Part 1: {} {}".format(station, amtInLOS))
+
+# This assumes asteroids in LOS > 200 otherwise we'd have to rotate more
+
+# Explanation:
+# The inversion of atan2 arguments orients the "unit circle" making the
+# y axis go from left to right and the x axis from top to bottom
+# the highest radian values will then be the ones straight above the source
+# Example: atan2(0, -1) = PI (Rule: atan2(y, x) <= PI)
+destroyed = [(math.atan2(dx, dy), (dx, dy)) for dx, dy in inLOS]
+destroyed.sort(reverse=True)
+dx, dy = destroyed[199][1]
+x, y = station[0]+dx, station[1]+dy
+while (x, y) not in asteroids:
+    x, y = x+dx, y+dy
+
+print("Part 2: {} {}".format((x, y), x*100 + y))
