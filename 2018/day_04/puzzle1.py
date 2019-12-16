03c169ee75553de8546cfe7f291503c13aaf0e43
Linus Probert <linus.probert@verisure.com>
2019-12-16 15:26:02 +0100

    Solves day 4 2018

--- 2018/day_04/puzzle1.py
+++ 2018/day_04/puzzle1.py
@@ -10,33 +10,42 @@ with open("input.txt") as fh:
 # Read data
 logs = []
 for line in raw_data:
-    raw = line.strip().split(" ", 2)
+    raw = line.rstrip().split(" ", 2)
     dateStr = " ".join(raw[:2])
     message = " ".join(raw[2:])
     data = (datetime.strptime(dateStr, "[%Y-%m-%d %H:%M]"), message)
     logs.append(data)
 
 # Sort the data
-logs = sorted(logs, key=lambda tup: tup[0])
-
+logs.sort()
 
 # Parse guard data
 guards = defaultdict(lambda:defaultdict(lambda:0))
 guard = None
+sleep = -1
 for log in logs:
     tstamp, message = log
     if message == "falls asleep":
-        guards[guard][tstamp.minute] += 1
+        sleep = tstamp.minute
     elif message == "wakes up":
-        pass
+        assert sleep >= 0
+        for m in range(sleep, tstamp.minute):
+            guards[guard][m] += 1
     else:
-        guard = message.split(" ")[1][1:]
+        guard = int(message.split(" ")[1][1:])
 
 # Find guard with highest sleeping rate and at what minute
+best_result = (0, 0, 0, 0)
+for guard, minutes in guards.items():
+    total_sleep = sum(minutes.values())
+    if best_result[1] < total_sleep:
+        best_minute = max(minutes, key=minutes.get)
+        best_result = (guard, total_sleep, best_minute, minutes[best_minute])
+print("Part 1:", best_result[0] * best_result[2])
+
 best_result = (0, 0, 0)
 for guard, minutes in guards.items():
     best_minute = max(minutes, key=minutes.get)
-    if minutes[best_minute] > best_result[2]:
+    if best_result[2] < minutes[best_minute]:
         best_result = guard, best_minute, minutes[best_minute]
-print(best_result)
-print("Part 1:", int(best_result[0]) * int(best_result[1]))
+print("Part 2:", best_result[0] * best_result[1])
