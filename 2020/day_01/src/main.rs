9311a3e5c8ccabbc18f957d2331c8ba1dc84d609
Linus Probert <linus.probert@gmail.com>
2020-12-01 06:18:23 +0100

    Completes day 1 2020

--- 2020/day_01/src/main.rs
+++ 2020/day_01/src/main.rs
@@ -1,6 +1,29 @@
+fn part1(input: &Vec<u32>) {
+    for (i1, n1) in input.iter().enumerate() {
+        for n2 in &input[i1..] {
+            if n1 + n2 == 2020 {
+                println!("Part1: {} * {} = {}", n1, n2, n1*n2);
+                return;
+            }
+        }
+    }
+}
+
+fn part2(input: &Vec<u32>) {
+    for (i1, n1) in input.iter().enumerate() {
+        for (i2, n2) in input[i1..].iter().enumerate() {
+            for n3 in input[i2..].iter() {
+                if n1 + n2 + n3 == 2020 {
+                    println!("Part2: {} * {} * {} = {}", n1, n2, n3, n1*n2*n3);
+                    return;
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let input = input_reader::get_inputs::<u32>();
-    for num in input {
-        println!("{}", num);
-    }
+    part1(&input);
+    part2(&input);
 }
