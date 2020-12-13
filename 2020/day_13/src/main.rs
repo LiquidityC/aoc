ee1cfc2e8416f50d79ba4bf227baf96f570aa5e9
Linus Probert <linus.probert@gmail.com>
2020-12-13 08:14:34 +0100

    Formats day 13 2020

--- 2020/day_13/src/main.rs
+++ 2020/day_13/src/main.rs
@@ -1,14 +1,21 @@
 fn part1(ts: u64, busline: &str) {
-    let busses: Vec<u64> = busline.split(",").filter(|b| *b != "x").map(|b| b.parse::<u64>().expect("Invalid bus")).collect();
-    let wait_times: Vec<(u64, u64)> = busses.iter().map(|t| {
-        (*t, t - (ts % t))
-    }).collect();
+    let busses: Vec<u64> = busline
+        .split(",")
+        .filter(|b| *b != "x")
+        .map(|b| b.parse::<u64>().expect("Invalid bus"))
+        .collect();
+    let wait_times: Vec<(u64, u64)> = busses.iter().map(|t| (*t, t - (ts % t))).collect();
     let min_wait = wait_times.iter().min_by(|b1, b2| b1.1.cmp(&b2.1)).unwrap();
     println!("Part 1: {}", min_wait.0 * min_wait.1);
 }
 
 fn part2(busline: &str) {
-    let times: Vec<(u64, u64)> = busline.split(",").enumerate().filter(|(_, b)| *b != "x").map(|(i, b)| (i as u64, b.parse::<u64>().unwrap())).collect();
+    let times: Vec<(u64, u64)> = busline
+        .split(",")
+        .enumerate()
+        .filter(|(_, b)| *b != "x")
+        .map(|(i, b)| (i as u64, b.parse::<u64>().unwrap()))
+        .collect();
     let mut stime: u64 = 0;
     let mut incr = times[0].1;
 
@@ -16,7 +23,7 @@ fn part2(busline: &str) {
         loop {
             stime += incr;
             if (stime + offset) % bus == 0 {
-                break
+                break;
             }
         }
         incr = incr * bus;
