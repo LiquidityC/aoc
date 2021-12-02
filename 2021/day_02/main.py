551ae41b20cb2e2024363f1d17a873830748a171
Linus Probert <linus.probert@gmail.com>
2021-12-02 08:49:29 +0100

    Code cleanup 2021 day 2

--- 2021/day_02/main.py
+++ 2021/day_02/main.py
@@ -1,37 +1,35 @@
 import sys
 
-def part1(data):
+def part1(moves):
     azimuth = 0
     depth = 0
-    for l in data:
-        #print(l, depth, azimuth)
-        [motion, count] = l.split()
+    for m in moves:
+        (motion, count) = m
         if motion == "forward":
-            azimuth += int(count)
+            azimuth += count
         elif motion == "down":
-            depth += int(count)
+            depth += count
         elif motion == "up":
-            depth -= int(count)
+            depth -= count
         else:
             print("Bad move: %s" % [motion, count])
             sys.exit(1)
 
     print("Part 1: %d" % (depth * azimuth))
 
-def part2(data):
+def part2(moves):
     azimuth = 0
     depth = 0
     aim = 0
-    for l in data:
-        #print(l, depth, azimuth)
-        [motion, count] = l.split()
+    for m in moves:
+        (motion, count) = m
         if motion == "forward":
-            azimuth += int(count)
-            depth += aim * int(count)
+            azimuth += count
+            depth += aim * count
         elif motion == "down":
-            aim += int(count)
+            aim += count
         elif motion == "up":
-            aim -= int(count)
+            aim -= count
         else:
             print("Bad move: %s" % [motion, count])
             sys.exit(1)
@@ -41,5 +39,6 @@ def part2(data):
 with open("input.txt") as fh:
     data = [l.strip() for l in fh.readlines()]
 
-part1(data)
-part2(data)
+moves = [(m, int(c)) for m,c in [l.split() for l in data]]
+part1(moves)
+part2(moves)
