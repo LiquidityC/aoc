7f9e5bd1db4362d6c9e973638a1784a14d726d4c
lprobert <linus.probert@volvocars.com>
2021-12-10 14:29:04 +0100

    2015 day 14

---
+++ 2015/day_14/src/main.c
@@ -0,0 +1,136 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+const char *input_file = "input.txt";
+
+typedef struct {
+    char name[10];
+    int speed;
+    int time;
+    int rest;
+    int distance;
+    int resting;
+    int moving;
+    int score;
+} Deer;
+
+int count_lines(FILE *fp)
+{
+    int count = 0;
+    int ch = 0;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '\n') {
+            count += 1;
+        }
+    }
+    rewind(fp);
+    return count;
+}
+
+Deer* read_deer(FILE *fp, int count)
+{
+    Deer *deer = (Deer*) malloc(sizeof(Deer)*count);
+    if (feof(fp)) {
+        fprintf(stderr, "File pointer is exhausted");
+    }
+
+    int index = 0;
+    while (index < count) {
+        Deer *tmp = &deer[index];
+        tmp->distance = 0;
+        tmp->resting = 0;
+        tmp->moving = 0;
+        tmp->score = 0;
+        fscanf(fp,
+               "%s can fly %d km/s for %d seconds, but then must rest for %d %*s",
+              tmp->name,
+               &tmp->speed,
+               &tmp->time,
+               &tmp->rest
+              );
+        index++;
+    }
+    return deer;
+}
+
+void part1(Deer *deer, int deer_count, int time)
+{
+    int top_distance = 0;
+    Deer *leader = NULL;
+
+    for (int i = 0; i < deer_count; ++i) {
+        int distance = 0;
+        Deer *tmp = &deer[i];
+        int full_cycles = time/(tmp->time + tmp->rest);
+        distance += full_cycles * tmp->speed * tmp->time;
+        int remainder = time % (tmp->time + tmp->rest);
+        if (remainder < tmp->time) {
+            distance += remainder * tmp->speed;
+        } else {
+            distance += tmp->time * tmp->speed;
+        }
+        if (distance > top_distance) {
+            top_distance = distance;
+            leader = &deer[i];
+        }
+    }
+
+    printf("Part 1: %d\n", top_distance);
+}
+
+void part2(Deer *deer, int deer_count, int time)
+{
+    int best_score = 0;
+    for (int step = 0; step < time; step++) {
+        int leading_distance = 0;
+        for (int i = 0; i < deer_count; i++) {
+            Deer *current = &deer[i];
+            if (current->resting == 0) {
+                current->distance += current->speed;
+                current->moving += 1;
+                if (current->moving >= current->time) {
+                    current->resting = current->rest;
+                    current->moving = 0;
+                }
+            } else {
+                current->resting -= 1;
+            }
+
+            if (current->distance > leading_distance) {
+                leading_distance = current->distance;
+            }
+        }
+        for (int i = 0; i < deer_count; i++) {
+            if (leading_distance == deer[i].distance) {
+                deer[i].score += 1; 
+            }
+            if (deer[i].score > best_score) {
+                best_score = deer[i].score;
+            }
+        }
+    }
+    printf("Part 2: %d\n", best_score);
+}
+
+int main(int argc, char** argv)
+{
+    FILE *fp = fopen(input_file, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to open file: %s", input_file);
+    }
+
+    int deer_count = count_lines(fp);
+    Deer *deer = read_deer(fp, deer_count);
+
+    part1(deer, deer_count, 2503);
+    part2(deer, deer_count, 2503);
+
+    free(deer);
+}
