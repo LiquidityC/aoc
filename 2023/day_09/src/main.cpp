d8692d483321df8934fc2a08b0d2f54de8c58823
Linus Probert <linus.probert@gmail.com>
2023-12-09 12:48:57 +0100

    Switch day 9 to int repor just for clarity

--- 2023/day_09/src/main.cpp
+++ 2023/day_09/src/main.cpp
@@ -13,18 +13,16 @@ using std::cout;
 using std::endl;
 using std::string;
 
-typedef uint32_t u32;
-
-static std::vector<std::vector<u32>> read_lines()
+static std::vector<std::vector<int>> read_lines()
 {
     std::ifstream in("input.txt");
 
-    std::vector<std::vector<u32>> data;
+    std::vector<std::vector<int>> data;
     string line;
     while (std::getline(in, line)) {
-        std::vector<u32> numbers;
+        std::vector<int> numbers;
         std::stringstream ss(line);
-        u32 num;
+        int num;
         while (ss >> num) {
             numbers.push_back(num);
         }
@@ -35,13 +33,13 @@ static std::vector<std::vector<u32>> read_lines()
     return data;
 }
 
-static u32 extrapolate_last(std::vector<u32>& numbers)
+static int extrapolate_last(std::vector<int>& numbers)
 {
     if (std::all_of(numbers.begin(), numbers.end(), [](const auto& a) { return a == 0; })) {
         return 0;
     }
 
-    std::vector<u32> next;
+    std::vector<int> next;
     next.reserve(numbers.size() - 1);
     for (size_t i = 0; i < numbers.size() - 1; ++i) {
         next.push_back(numbers[i+1] - numbers[i]);
@@ -49,23 +47,23 @@ static u32 extrapolate_last(std::vector<u32>& numbers)
     return numbers.back() + extrapolate_last(next);
 }
 
-static void part1(std::vector<std::vector<u32>>& data)
+static void part1(std::vector<std::vector<int>>& data)
 {
-    u32 total = 0;
+    int total = 0;
     for (auto& numbers : data) {
-        u32 next = extrapolate_last(numbers);
+        int next = extrapolate_last(numbers);
         total += next;
     }
     cout << "Part 1: " << total << endl;
 }
 
-static int extrapolate_first(std::vector<u32>& numbers)
+static int extrapolate_first(std::vector<int>& numbers)
 {
     if (std::all_of(numbers.begin(), numbers.end(), [](const auto& a) { return a == 0; })) {
         return 0;
     }
 
-    std::vector<u32> next;
+    std::vector<int> next;
     next.reserve(numbers.size() - 1);
     for (size_t i = 0; i < numbers.size() - 1; ++i) {
         next.push_back(numbers[i+1] - numbers[i]);
@@ -73,11 +71,11 @@ static int extrapolate_first(std::vector<u32>& numbers)
     return numbers.front() - extrapolate_first(next);
 }
 
-static void part2(std::vector<std::vector<u32>>& data)
+static void part2(std::vector<std::vector<int>>& data)
 {
-    u32 total = 0;
+    int total = 0;
     for (auto& numbers : data) {
-        u32 next = extrapolate_first(numbers);
+        int next = extrapolate_first(numbers);
         total += next;
     }
     cout << "Part 2: " << total << endl;
