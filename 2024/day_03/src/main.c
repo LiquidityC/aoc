3e2653bc2cabb0cc1dfc91dbc1463aeeaea4cde6
Linus Probert <linus.probert@gmail.com>
2024-12-04 14:40:16 +0100

    2024 day 03 cleanup
    No reason to re-write just nice to re-factor some code.

--- 2024/day_03/src/main.c
+++ 2024/day_03/src/main.c
@@ -6,84 +6,103 @@
 #include <stdint.h>
 #include <string.h>
 
+typedef struct {
+    uint32_t sum;
+    bool do_mul;
+} State;
+
+typedef struct {
+    const char *match;
+    const size_t mlen;
+    void (*exec)(State *state, char **pos);
+} Operation;
+
+#define OP_CREATE(pattern, func) { .match = pattern, .mlen = strlen(pattern), .exec = func }
+
+static void mul_exec(State *state, char **pos)
+{
+    uint32_t a, b;
+
+    *pos += 4;
+
+    if (!state->do_mul) {
+        return;
+    }
+
+    a = strtol(*pos, pos, 10);
+    if (a == 0 || **pos != ',') {
+        return;
+    }
+    *pos += 1;
+    b = strtol(*pos, pos, 10);
+    if (b == 0 || **pos != ')') {
+        return;
+    }
+
+    state->sum += a * b;
+}
+
+static void do_exec(State *state, char **pos)
+{
+    state->do_mul = true;
+    *pos += 4;
+}
+
+static void dont_exec(State *state, char **pos)
+{
+    state->do_mul = false;
+    *pos += 6;
+}
+
 static void part1(Lines *lines)
 {
-    uint32_t sum = 0;
+    const Operation mul_operation = OP_CREATE("mul(", mul_exec);
+    State state = { .sum = 0, .do_mul = true };
 
     for (size_t i = 0; i < lines->size; i++) {
         char *line = lines->lines[i];
-        uint32_t a, b;
         char *pos = line;
 
-        while (true) {
-            pos = strstr(pos, "mul(");
-            if (pos == NULL) {
-                break;
-            }
-
-            /* Skip the "mul(" */
-            pos += 4;
-
-            /* Rad first number */
-            a = strtol(pos, &pos, 10);
-            if (*pos != ',') {
-                continue;
-            }
-
-            /* Skip the "," */
-            pos += 1;
-
-            /* Read second number */
-            b = strtol(pos, &pos, 10);
-
-            /* Confirm the closing parenthesis */
-            if (*pos != ')') {
-                continue;
-            }
-
-            sum += a * b;
-        }
-    }
-
-    printf("Part 1: %u\n", sum);
-}
-
-static void part2(Lines *lines)
-{
-    uint32_t sum = 0;
-    bool do_mul = true;
-
-    for (size_t i = 0; i < lines->size; ++i) {
-        char *pos = lines->lines[i];
-
-        uint32_t a, b;
         while (*pos != '\0') {
-            if (do_mul && strncmp(pos, "mul(", 4) == 0) {
-                pos += 4;
-                a = strtol(pos, &pos, 10);
-                if (*pos != ',') {
-                    continue;
-                }
-                pos += 1;
-                b = strtol(pos, &pos, 10);
-                if (*pos != ')') {
-                    continue;
-                }
-                sum += a * b;
-                pos += 1;
-            } else if (strncmp(pos, "do()", 3) == 0) {
-                do_mul = true;
-                pos += 4;
-            } else if (strncmp(pos, "don't()", 7) == 0) {
-                do_mul = false;
-                pos += 7;
+            if (strncmp(mul_operation.match, pos, mul_operation.mlen) == 0) {
+                mul_operation.exec(&state, &pos);
             } else {
                 pos += 1;
             }
         }
     }
 
-    printf("Part 2: %u\n", sum);
+    printf("Part 1: %u\n", state.sum);
+}
+
+static void part2(Lines *lines)
+{
+    const Operation ops[] = {
+        OP_CREATE("mul(", mul_exec),
+        OP_CREATE("do()", do_exec),
+        OP_CREATE("don't()", dont_exec),
+    };
+
+    State state = { .sum = 0, .do_mul = true };
+
+    for (size_t i = 0; i < lines->size; ++i) {
+        char *pos = lines->lines[i];
+
+        while (*pos != '\0') {
+            bool match = false;
+            for (size_t j = 0; j < sizeof(ops) / sizeof(ops[0]); ++j) {
+                if (strncmp(ops[j].match, pos, ops[j].mlen) == 0) {
+                    ops[j].exec(&state, &pos);
+                    match = true;
+                }
+            }
+            if (!match) {
+                pos += 1;
+            }
+        }
+    }
+
+    printf("Part 2: %u\n", state.sum);
 }
 
 int main(int argc, char **argv)
